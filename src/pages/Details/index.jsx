9b191cc816707385c22e4eac272b22491bdaf42b
Gabriel <gabrielloures21@gmail.com>
2022-12-14 10:00:55 -0300

    End Details Front

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,7 +1,17 @@
 import { Header } from "../../components/Header"
 import { Footer } from "../../components/Footer"
+import { Button } from "../../components/Button"
 
-import { Container } from "./styles"
+import { MdOutlineArrowBackIos } from "react-icons/md"
+
+import salad from "../../assets/ravanello.png"
+import lettuce from "../../assets/alface.png"
+import tomato from "../../assets/tomate.png"
+import radish from "../../assets/rabanete.png"
+import naan from "../../assets/naan.png"
+import order from "../../assets/order-logo.svg"
+
+import { Container, ContentHeader, Content, MainContent } from "./styles"
 
 export function Details() {
 
@@ -10,6 +20,67 @@ export function Details() {
     <Container>
 
       <Header/>
+
+      <Content>
+
+        <ContentHeader>
+          <a href="#">
+            <MdOutlineArrowBackIos/>
+            <span>Voltar</span>
+          </a>
+        </ContentHeader>
+
+        <MainContent>
+          <img src={salad} alt="Salada Ravanello" />
+          <div className="description">
+
+            <div className="texts">
+              <h1>Salada Ravanello</h1>
+              <p>Rabanetes, folhas verdes e molho agridoce salpicados com gergelim</p>
+            </div>
+
+            <div className="products-list">
+              <ul>
+                <li>
+                  <img src={lettuce} alt="Alface" />
+                  <p>Alface</p>
+                </li>
+                <li>
+                  <img src={tomato} alt="Tomate" />
+                  <p>Tomate</p>
+                </li>
+                <li>
+                  <img src={radish} alt="Rabante" />
+                  <p>Rabante</p>
+                </li>
+                <li>
+                  <img src={naan} alt="Pão Naan" />
+                  <p>Pão Naan</p>
+                </li>
+              </ul>
+            </div>
+
+            <div className="payments-details">
+
+              <div className="value">
+                <h1>R$</h1> 
+                <h1>25,97</h1>
+              </div>
+
+              <div className="plus-minus">
+                <h5>-</h5>
+                <h5>01</h5>
+                <h5>+</h5>
+              </div>
+              
+              <Button icon={order} title="Incluir"/>
+
+            </div>
+
+          </div>
+        </MainContent>
+
+      </Content>
 
       <Footer/>
 
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -12,4 +12,141 @@ export const Container = styled.div`
   "content"
   "footer"
 
+`;
+
+export const Content = styled.div`
+
+  grid-area: content;
+
+  padding: 25px 300px 168px 130px;
+
+`;
+
+export const ContentHeader = styled.div`
+
+  width: 100%;
+
+  > a {
+    display: flex;
+    gap: 11px;
+    align-items: center;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 140%;
+    color: ${({ theme }) => theme.COLORS.GRAY_H1};
+  }
+
+`;
+
+export const MainContent = styled.div`
+
+  display: flex;
+  align-items: center;
+  gap: 200px;
+  margin-top: 55px;
+
+  > img {
+    width: 545px;
+    height: 545px;
+  }
+
+  > .description .texts {
+    h1 {
+      font-family: 'Poppins';
+      font-style: normal;
+      font-weight: 500;
+      font-size: 40px;
+      line-height: 140%;
+      color: ${({ theme }) => theme.COLORS.GRAY_H1};
+
+      margin-bottom: 8px;
+    }
+
+    p {
+      font-family: 'Poppins';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 24px;
+      line-height: 140%;
+      color: ${({ theme }) => theme.COLORS.GRAY_H1};
+    }
+  }
+
+  > .description .products-list {
+
+    margin-top: 35px;
+
+    ul {
+      display: flex;
+      list-style: none;
+    }
+
+    ul li {
+      margin-right: 30px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+
+    ul li p {
+      font-family: 'Poppins';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 18px;
+      line-height: 140%;
+      color: ${({ theme }) => theme.COLORS.GRAY_H1};
+
+      margin-top: 8px;
+    }
+
+    img {
+      width: 100px;
+      height: 100px;
+    }
+  }
+
+  > .description .payments-details {
+    display: flex;
+    align-items: center;
+    gap: 100px;
+    margin-top: 45px;
+
+    .value {
+      display: flex;
+      gap: 10px;
+    }
+
+    .value h1 {
+      font-family: 'Roboto';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 32px;
+      line-height: 160%;
+      color: ${({ theme }) => theme.COLORS.BLUE};
+
+      text-align: center;
+    }
+
+    .plus-minus {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 17.5px;
+    }
+
+    .plus-minus h5 {
+      font-family: 'Roboto';
+      font-style: normal;
+      font-weight: 700;
+      font-size: 30px;
+      line-height: 160%;
+
+      color: ${({ theme }) => theme.COLORS.GRAY_H1};
+    }
+  }
+  
+
 `
+
