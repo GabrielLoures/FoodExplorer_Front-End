0afb53db740a7a7ac2b56028717d112a009322d3
Gabriel <gabrielloures21@gmail.com>
2023-01-10 17:23:38 -0300

    Changed api.js baseURL to Render

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  // baseURL: "https://foodexplorer-api-k8d5.onrender.com"
+  baseURL: "https://foodexplorer-api-k8d5.onrender.com"
 
-  baseURL: "http://localhost:3333"
+  // baseURL: "http://localhost:3333"
 })
 
 // SITE: foodexplorer2.netlify.app
